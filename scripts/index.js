2cac503c19fb10d737f108cd129bb632ef65b68d
Gzman <ganzmann@protonmail.com>
2021-04-13 15:39:08 +0200

    Added clear button to clear the grid

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,20 @@
 
-const gridContainer = document.querySelector(".container");
+const grid = document.querySelector(".grid");
+const clearButton = document.querySelector(".clear-button");
+clearButton.addEventListener("click", clearGrid);
 
-for (let i = 0; i < 16 * 16; i++) {
-    let cell = document.createElement("div");
-    cell.style["height"] = "3rem";
-    cell.style["width"] = "3rem";
-    cell.style["border"] = "0.1rem solid grey";
-    cell.addEventListener("mouseenter", e => e.target.classList.add("cell-hover"));
-    gridContainer.append(cell);
+function initGrid() {
+    for (let i = 0; i < 16 * 16; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener("mouseenter", e => e.target.classList.add("cell-hover"));
+        grid.append(cell);
+    }
 }
+
+function clearGrid() {
+    const cells = grid.querySelectorAll("div");
+    cells.forEach(cell => cell.classList.remove("cell-hover"));
+}
+
+initGrid();
