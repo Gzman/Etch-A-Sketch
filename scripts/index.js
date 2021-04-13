37143cf6c3af62e1ffe202a4195ac2f919cbf700
Gzman <ganzmann@protonmail.com>
2021-04-13 15:04:59 +0200

    Added code to display square grid

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+
+const gridContainer = document.querySelector(".container");
+
+for (let i = 0; i < 16 * 16; i++) {
+    let cell = document.createElement("div");
+    cell.style["height"] = "3rem";
+    cell.style["width"] = "3rem";
+    cell.style["border"] = "0.1rem solid grey";
+    gridContainer.append(cell);
+}
