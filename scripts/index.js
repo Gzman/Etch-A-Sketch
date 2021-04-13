7910b64d48ec51496dcfd9b121afbd5bcd911a06
Gzman <ganzmann@protonmail.com>
2021-04-13 15:07:14 +0200

    grid-cells can now be colored.

--- scripts/index.js
+++ scripts/index.js
@@ -6,5 +6,6 @@ for (let i = 0; i < 16 * 16; i++) {
     cell.style["height"] = "3rem";
     cell.style["width"] = "3rem";
     cell.style["border"] = "0.1rem solid grey";
+    cell.addEventListener("mouseenter", e => e.target.classList.add("cell-hover"));
     gridContainer.append(cell);
 }
